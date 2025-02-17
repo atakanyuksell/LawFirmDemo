2d3ae4e41466af4e7ef5b37c8ba2c17a2101116f
atakanyuksell <atakanyukselis@hotmail.com>
2025-02-17 23:19:44 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Menü butonuna tıklanınca menüyü aç/kapat
+document.querySelector(".menu-icon").addEventListener("click", function () {
+    document.querySelector(".navbar").classList.toggle("active");
+  });
+  
